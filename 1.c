17dd99ff7903407b3de1fed51fd80429c2c2eb53
barbiyong <barbiyong@hotmail.com>
2016-04-25 11:40:59 +0700

    First add 3 files

---
+++ 1.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+void main()
+{
+  int testFile1[10] = 0 ; 
+  printf("Bye Bye\n"); 
+}
---
+++ 2.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+void main()
+{
+  int testFile2[10] = 0 ; 
+  printf("Bye Bye\n"); 
+}
